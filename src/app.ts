2803556ef143cd828b57b473006bcc5d26a1c59b
Steven Dubé <77smu19@protonmail.com>
2024-02-13 19:04:13 -0500

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express, { Express } from "express";
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const cookieParser = require("cookie-parser");
-const cors = require('cors')
+const cors = require('cors');
 const app: Express = express();
 app.use(express.json());
 
 app.use(cookieParser());
+app.use(cors());
 app.use('/tours', tourRouter);
 app.use('/users', userRouter);
 
